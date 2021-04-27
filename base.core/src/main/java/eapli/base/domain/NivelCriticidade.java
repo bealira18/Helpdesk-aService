ad008fbd15da762c92884bd7c2ebcf9fa09837da
bealira <1200607@isep.ipp.pt>
2021-04-27 10:50:29 +0100

    <LAPR4-11> UC-B-1003 #comment fim criação tabela nivel criticidade

--- base.core/src/main/java/eapli/base/domain/NivelCriticidade.java
+++ base.core/src/main/java/eapli/base/domain/NivelCriticidade.java
@@ -15,10 +15,10 @@ public class NivelCriticidade implements AggregateRoot<Integer>, Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String objetivo;
-    private Tempo tempoMedioA;
-    private Tempo tempoMaximoA;
-    private Tempo tempoMedioR;
-    private Tempo tempoMaximoR;
+    private int tempoMedioA;
+    private int tempoMaximoA;
+    private int tempoMedioR;
+    private int tempoMaximoR;
     private Valor valor;
     private Etiqueta etiqueta;
 
@@ -27,10 +27,11 @@ public class NivelCriticidade implements AggregateRoot<Integer>, Serializable {
 
     public NivelCriticidade(String objetivo, int tempoMaximoA, int tempoMaximoR, int valor, String etiqueta) {
         this.objetivo = objetivo;
-        this.tempoMedioA = new Tempo(0);
-        this.tempoMaximoA = new Tempo(tempoMaximoA);
-        this.tempoMedioR = new Tempo(0);
-        this.tempoMaximoR = new Tempo(tempoMaximoR);
+        Tempo t=new Tempo(0,tempoMaximoA,0,tempoMaximoR);
+        this.tempoMedioA = t.getTempoMedioA();
+        this.tempoMaximoA = t.getTempoMaximoA();
+        this.tempoMedioR = t.getTempoMedioR();
+        this.tempoMaximoR = t.getTempoMaximoR();
         this.valor= new Valor(valor);
         this.etiqueta= new Etiqueta(etiqueta);
 
