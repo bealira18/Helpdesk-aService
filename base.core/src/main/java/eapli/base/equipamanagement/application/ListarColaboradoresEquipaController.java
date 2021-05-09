b02ad2a466b8edb31e4b71cbc976cdeda7552e18
Gonçalo Corredoura <1190617@isep.ipp.pt>
2021-05-09 17:52:44 +0100

    <LAPR4-184> Correção do código de listar colaboradores de uma equipa.

---
+++ base.core/src/main/java/eapli/base/equipamanagement/application/ListarColaboradoresEquipaController.java
@@ -0,0 +1,31 @@
+package eapli.base.equipamanagement.application;
+
+import eapli.base.colaboradormanagement.domain.Colaborador;
+import eapli.base.equipamanagement.domain.Equipa;
+import eapli.base.equipamanagement.repository.EquipaRepository;
+import eapli.base.infrastructure.persistence.PersistenceContext;
+
+import java.util.List;
+
+public class ListarColaboradoresEquipaController {
+
+    private final EquipaRepository equipaRepository= PersistenceContext.repositories().equipa();
+
+    public List<Colaborador> listarColaboradoresEquipa(String acronimo){
+
+        Iterable<Equipa> equipas = equipaRepository.findAll();
+        Equipa equipa = null;
+
+        for(Equipa e : equipas){
+            if(e.acronimo().stringAcronimo().equalsIgnoreCase(acronimo)){
+                equipa = e;
+            }
+        }
+
+        if(equipa == null){
+            throw new IllegalArgumentException("Não existe nenhuma equipa com o acrónimo: " + acronimo);
+        }
+
+        return equipa.colaboradores();
+    }
+}
