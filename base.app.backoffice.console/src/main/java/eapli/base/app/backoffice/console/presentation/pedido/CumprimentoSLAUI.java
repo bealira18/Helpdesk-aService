a82544e3c6808f71c162b7e912bb731408e0d72e
bealira <1200607@isep.ipp.pt>
2021-06-22 00:39:36 +0100

    <LAPR4-184> #comment correção dos SLA's

--- base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/pedido/CumprimentoSLAUI.java
+++ base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/pedido/CumprimentoSLAUI.java
@@ -10,6 +10,8 @@ import eapli.base.tarefamanagement.domain.Tarefa;
 import eapli.framework.io.util.Console;
 import eapli.framework.presentation.console.AbstractUI;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -21,24 +23,36 @@ public class CumprimentoSLAUI extends AbstractUI {
     @Override
     protected boolean doShow(){
 
-        Date dataI= Console.readDate("\nData incial: ","dd/mm/yyyy");
-        Date dataF= Console.readDate("\nData incial: ","dd/mm/yyyy");
+        SimpleDateFormat DateFor = new SimpleDateFormat("dd/MM/yyyy");
+        Date dataI = new Date();
+        Date dataF = new Date();
+
+        String datai= Console.readLine("\nData incial (dd/MM/yyyy): ");
+        String dataf= Console.readLine("\nData incial dd/MM/yyyy: ");
+
+        try {
+            dataI=DateFor.parse(datai);
+            dataF=DateFor.parse(dataf);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
         List<Pedido> pedidos=listarIncumprimentoSLA.pedidosPorData(dataI,dataF);
         Iterable<Catalogo> catalogos=listarCatalogosController.listarCatalogos();
 
+
         for(Catalogo c : catalogos){
             System.out.println("Titulo Catalogo: "+c.obterTitulo());
             System.out.println("Niveis de Criticidade: "+c.obterNivelCriticidade());
-
-            for(Servico s : listarIncumprimentoSLA.ServicoPorCatalogo(c.obterId())){
-
-                for(Pedido p : listarIncumprimentoSLA.pedidosDeServico(s.obterId(),pedidos)){
-
-                    for(InfoTarefa t : p.obterListaTarefas()){
-
-                        System.out.println("Tempo decorrido"+t.obterTempoDecorrido());
-                        System.out.println("Tempo médio"+t.obterTarefa().obterTempoMedio());
+            Iterable<Servico> servicos=listarIncumprimentoSLA.ServicoPorCatalogo(c.obterId());
+            for(Servico s : servicos){
+                Iterable<Pedido> pd=listarIncumprimentoSLA.pedidosDeServico(s.obterId(),pedidos);
+                for(Pedido p : pd){
+                    Iterable<InfoTarefa> it=p.obterListaTarefas();
+                    for(InfoTarefa t : it){
+                        System.out.println("Tarefa: "+t.obterId());
+                        System.out.println("Tempo decorrido: "+t.obterTempoDecorrido());
+                        System.out.println("Tempo médio: "+t.obterTarefa().obterTempoMedio()+"\n");
                     }
                 }
             }
